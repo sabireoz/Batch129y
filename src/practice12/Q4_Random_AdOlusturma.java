7ab6882fe2a1e38c11ab1e920f81c5c4479ec569
Sabire Ozkan <ozkan.sabire2020@gmail.com>
2023-01-28 19:52:14 +0200

    first commit

---
+++ src/practice12/Q4_Random_AdOlusturma.java
@@ -0,0 +1,36 @@
+package practice12;
+
+public class Q4_Random_AdOlusturma {
+
+//Rastgele alinan harfleri bir stringe ekleyerek adinizi olusturan ve bunu kac deneme
+
+
+      //  public class Q04_Random_AdOlusturma {
+
+            //Rastgele alınan harfleri bir Stringe ekleyerek adınızı oluşturan ve bunu kaç deneme ile yaptığını yazdıran bir kod yazınız.
+
+            public static void main(String[] args) {
+                String alfabe = "abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ ";
+                String isim = "Ali Can";
+                String yeniIsim = "";
+
+                int sayac = 0;
+
+                for (int i = 0; i < isim.length(); i++) {
+
+                    while (true) {
+                        char rastgeleHarf = alfabe.charAt((int) (Math.random() * alfabe.length()));
+                        sayac++;
+                        if (isim.charAt(i) == rastgeleHarf) {
+                            yeniIsim += rastgeleHarf;
+                            System.out.println("yeniIsim = " + yeniIsim);
+                            System.out.println("sayac = " + sayac);
+                            break;
+                        }
+                    }
+                }
+
+                System.out.println("sayac = " + sayac);
+            }
+
+        }
