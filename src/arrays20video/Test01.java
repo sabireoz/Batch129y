81dc2cf010230e38c6f11b05d91f0250581b6f50
Sabire Ozkan <ozkan.sabire2020@gmail.com>
2023-02-14 17:44:35 +0200

    v1.0.0

--- src/arrays20video/Test01.java
+++ src/arrays20video/Test01.java
@@ -2,7 +2,9 @@ package arrays20video;
 
 public class Test01 {
 
-
+    public static void main(String[] args) {
+        System.out.println("deneme 01");
+    }
 
     //21video
     //listler eleman sayisinda esnktir arrayler degil
