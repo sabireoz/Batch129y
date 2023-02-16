b9c5e190c2d6eb48bea35fbd2feb4a22efb2fa2b
Sabire Ozkan <ozkan.sabire2020@gmail.com>
2023-02-16 22:05:06 +0200

    v1.0.0.1

---
+++ src/RestaurantBillGenerator/Dish.java
@@ -0,0 +1,44 @@
+package RestaurantBillGenerator;
+
+public class Dish {
+
+
+
+    //3
+
+        //yiyeceklerin fieldlarına okunsun ancak değiştirilemesin
+        private int code;
+        private String name;
+        private double price;
+
+        //yiyecek oluşturulurken özelllikler set edilsin
+        public Dish(int code, String name, double price) {
+            this.code = code;
+            this.name = name;
+            this.price = price;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        //dish objesini yazdırmak için
+        @Override
+        public String toString() {
+            return  "code=" + code +
+                    ", name='" + name + '\'' +
+                    ", price=" + price +
+                    '}';
+        }
+    }
+
+
+
